0a75035ccca415cfdadecefbf2573809b8254a40
Aura Herrera <aursolis@amazon.com>
2018-11-16 20:22:39 +0000

    Typo in index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World - I'm DEV',
+        'output': 'Hello World - I am DEV',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
